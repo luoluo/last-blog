af615f5081130845d05dd21144eaf7d3308c3f62
luoluo <luoluo1920@gmail.com>
2017-02-10 22:50:52 -0500

    add more files

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import flask
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
+
+
+@app.route("/test_render")
+def test_render():
+    return render_template("hello.html")
+
+@app.route("/user/<name>")
+def hello_you(name):
+    return "Hello {}!".format(name)
 
 @app.route("/")
 def hello():
