8989916d9459e504cf890bbca69d4ac7f5ea98e7
luoluo <luoluo1920@gmail.com>
2017-02-12 05:00:11 -0500

    add more template && update css

--- main.py
+++ main.py
@@ -2,25 +2,40 @@ import flask
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import url_for
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    # return render_template("about.html")
+    return redirect(url_for("construct"))
 
+@app.route("/archives/")
+def archives():
+    return render_template("archives.html")
 
-@app.route("/test_render")
-def test_render():
-    return render_template("hello.html")
+@app.route("/archives/<string:archive_id>")
+def archive(archive_id):
+    return render_template("post.html")
 
-@app.route("/user/<name>")
-def hello_you(name):
-    return "Hello {}!".format(name)
+@app.route("/construct")
+def construct():
+    return render_template("construct.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("construct.html")
+
+@app.route("/dev")
+def dev():
+    return render_template("archives.html")
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    #return redirect(url_for("construct"))
+    return redirect(url_for("dev"))
 
 if __name__ == "__main__":
     app.run()
